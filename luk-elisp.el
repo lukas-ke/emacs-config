656239beff10e0e0f0f709f0acb55fc495be0fd8
lukas-ke <lukas-ke@users.noreply.github.com>
2021-04-18 15:53:36 +0200

    Auto-insert coding and lexical binding comment in new elisp files
    Add new function luk-elisp-maybe-insert-boilerplate to the
    emacs-lisp-mode-hook, to insert the special comment to ensure utf-8
    file encoding and that the code is evaluated with lexical binding
    instead of dynamic binding.

--- luk-elisp.el
+++ luk-elisp.el
@@ -2,7 +2,24 @@
 
 (provide 'luk-elisp)
 
+(defun luk-elisp-insert-boilerplate ()
+  "Insert magic comment for utf-8-coding and lexical binding"
+  (interactive)
+  (save-excursion
+    (beginning-of-buffer)
+    (insert ";;; -*- coding: utf-8; lexical-binding: t -*-\n\n")))
+
+(defun luk-elisp-maybe-insert-boilerplate ()
+  "Run luk-elisp-insert-boilerplate for new Emacs-lisp files."
+  (when (and (buffer-file-name) (not (file-exists-p (buffer-file-name))) (= (point) 1))
+    (luk-elisp-insert-boilerplate)
+    (forward-line)
+    (forward-line)))
+
 (defun luk-elisp-setup ()
   (when (require 'company nil 'noerror)
     (add-hook 'emacs-lisp-mode-hook 'company-mode))
+
+  (add-hook 'emacs-lisp-mode-hook 'luk-elisp-maybe-insert-boilerplate)
+
   (define-key emacs-lisp-mode-map (kbd "C-c g") 'xref-find-definitions))
