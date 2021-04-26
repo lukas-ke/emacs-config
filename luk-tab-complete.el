3d8dd3cd5f8fe33f44c48d1fbf2763669329a9c0
lukas-ke <lukas-ke@users.noreply.github.com>
2021-04-26 22:19:43 +0200

    Rewrite luk-tab-complete-smart-tab with cond instead of ifs
    For less nesting.

--- luk-tab-complete.el
+++ luk-tab-complete.el
@@ -12,21 +12,40 @@ minibuffer. Else, if mark is active, indents region. Else if
 point is at the end of a symbol, expands it. Else indents the
 current line."
   (interactive)
-  (if (string-prefix-p "magit" (symbol-name major-mode))
-      ;; TODO: hack. Can I instead make magit override my otherwise global key?
-      (magit-section-toggle (magit-current-section))
-    (if (minibufferp)
-        (minibuffer-complete)
-      (if mark-active
-          (indent-region (region-beginning)
-                         (region-end))
-        (if (looking-at "\\_>") ;; At of word
-            (if (and (eq major-mode 'org-mode)
-                     (= ?* (char-before)))
-                ;; Don't tab complete after * in org, it gets weird.
-                ;; Better let org-cycle do its thing.
-                (org-cycle)
-              (dabbrev-expand nil))
-          (if (eq major-mode 'org-mode)
-              (org-cycle)
-            (indent-for-tab-command)))))))
+  (cond
+   ;; TODO: hack. Can I instead make magit override my otherwise
+   ;; global key?
+   ((string-prefix-p "magit" (symbol-name major-mode))
+    (magit-section-toggle (magit-current-section)))
+
+   ((minibufferp)
+    (minibuffer-complete))
+
+   (mark-active
+    (indent-region
+     (region-beginning)
+     (region-end)))
+
+   ;; At end of word, so *maybe* expand the word
+   ;; TODO: "\\_>" can give some issues because dabbrev-expand can
+   ;; insert symbols that prevent \\_> from matching (like .)
+   ;; and then further presses of <tab> no longer expands.
+   ;; maybe find a different way to check if at end of word, e.g.
+   ;; (point) is on space or \n and preceding is not space or \n.
+   ((looking-at "\\_>")
+    (if (and (eq major-mode 'org-mode) (= ?* (char-before)))
+        ;; Don't expand after * in org, it gets weird. Instead let
+        ;; org-cycle do its thing.
+        ;;
+        ;; TODO: Not perfect, if dabbrev expands "word" to "word*",
+        ;; tab will instead start cycling which is surprising.
+        (org-cycle)
+      (dabbrev-expand nil)))
+
+   ((eq major-mode 'org-mode)
+    ;; Not at end of word in org-mode -> org cycle
+    (org-cycle))
+
+   (t
+    ;; Not at end of word, just indent-for-tab
+    (indent-for-tab-command))))
