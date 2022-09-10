5f7366a9d2342d34f1b0b064e6a3056e3bc1825f
lukas-ke <lukas-ke@users.noreply.github.com>
2022-09-10 18:43:39 +0200

    Add lsp-settings file luk-lsp-conf
    - C-c g for "lsp-find-definition".
    - Disable the breadcrumb headerline.
    - Show doc for thing at cursor (that is, when the caret is on
      something documented).

--- luk-lsp-conf.el
+++ luk-lsp-conf.el
@@ -2,4 +2,20 @@
 (provide 'luk-lsp-conf)
 
 (defun luk-lsp-conf-setup ()
-  (define-key lsp-mode-map (kbd "C-c g") 'lsp-find-definition))
+  (define-key lsp-mode-map (kbd "C-c g") 'lsp-find-definition)
+
+  ;; lsp-mode requires yas-snippets for completion
+  ;; (Otherwise I get $0 when completing calls with arguments, e.g.
+  ;; some_func($0)|
+  (add-hook 'lsp-mode-hook 'yas-minor-mode)
+  (add-hook 'python-mode-hook 'lsp)
+
+  ;; Disable the File > Class > Function header.
+  ;; It's a little useful, but also kinda wasteful.
+  (setq lsp-headerline-breadcrumb-enable nil)
+
+  ;; Show the documentation popup when point is at something.
+  ;; This might drive me nuts, but I don't use the mouse.
+  ;;
+  ;; Maybe there's a better way to do this, e.g. some simple keypress.
+  (setq lsp-ui-doc-show-with-cursor t))
