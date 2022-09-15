fd01c4a3d54e60f3bfafaa290a64c0d49a4f32f2
lukas-ke <lukas-ke@users.noreply.github.com>
2022-09-15 07:10:23 +0200

    Add folder-up function and font replacement extensions

--- luk-font.el
+++ luk-font.el
@@ -2,9 +2,17 @@
 
 ;; Font replacement to support some symbols.
 
+;; TODO: Should I just set the entire segoe ui symbol private use area?
+
+(set-fontset-font "fontset-default" #xE117 "segoe ui symbol") ;; Refresh 
+(set-fontset-font "fontset-default" #xE1C9 "segoe ui symbol") ;; Cell phone 
+(set-fontset-font "fontset-default" #xE188 "segoe ui symbol") ;; Folder 
+(set-fontset-font "fontset-default" #xe107 "segoe ui symbol") ;; Trashcan 
+(set-fontset-font "fontset-default" #xe105 "segoe ui symbol") ;; Save 
 (set-fontset-font "fontset-default" #x1f512 "segoe ui symbol") ;; Padlock: 🔒
 (set-fontset-font "fontset-default" #x2bc1 "segoe ui symbol") ;; Diamond: ⯁
-(set-fontset-font t '(?😊 . ?😎) "Segoe UI Emoji") ;; Some emoji range)
+(set-fontset-font t ?💩 "Segoe UI Emoji") ;; Pile of poo 💩
+(set-fontset-font t '(?😊 . ?😎) "Segoe UI Emoji") ;; Some emoji range
 
 ;; github-octicons installed via all-the-icons
 (set-fontset-font t #xf020 "github-octicons") ;;  Git branch
---
+++ luk-util.el
@@ -0,0 +1,15 @@
+;;; -*- coding: utf-8; lexical-binding: t -*-
+
+(provide 'luk-util)
+
+(defun -up (&optional path n)
+  "Get the N-th parent folder of PATH or `buffer-file-name'."
+  (or path (setq path (buffer-file-name)))
+
+  (if n
+    (let ((folder path))
+      (dotimes (num n)
+        (setq path (file-name-directory (directory-file-name path))))
+      path)
+
+    (file-name-directory (directory-file-name path))))
