3c0e7fe62770ad79c07538d88da376aee6b28dbd
lukas-ke <lukas-ke@users.noreply.github.com>
2022-09-18 12:41:20 +0200

    Add considerate whitespace-trimming for markdown
    Add buffer-local variable "luk-should-trim-whitespace" which allows
    suppressing "luk-maybe-trim-whitespace" for certain modes.
    
    Remove the previous explicit exclude of "markdown-mode" from
    "luk-maybe-trim-whitespace", instead set "luk-should-trim-whitespace"
    to nil in the "markdown-mode-hook".
    
    Add command "luk-markdown-delete-trailing-whitespace", which deletes
    single trailing whitespace, more than two whitespaces (truncates down
    to two) or all whitespace on lines with only whitespace.
    
    Add that command to the before-save-hook in when markdown-mode.

--- luk-markdown.el
+++ luk-markdown.el
@@ -6,10 +6,42 @@
   "markdown-mode"
   "Major mode for editing Markdown files" t)
 
-(defun luk-markdown--line-wrap-off () (setq truncate-lines t))
+(defun luk-markdown-delete-trailing-whitespace ()
+  "Delete trailing respecting markdown hard line breaks.
+
+Markdown uses two spaces for a hard linebreak so this command:
+- deletes any single trailing space at line-ends,
+- leaves instances of exactly two trailing spaces unmodified,
+- trims more than two spaces to exactly two,
+- trims whitespace completely at lines with only spaces, regardless of count."
+  (interactive)
+  (save-match-data
+    (save-excursion
+      (beginning-of-buffer)
+      (while (re-search-forward "[ ]+$" nil t)
+        (let* ((b (match-beginning 0)) (e (match-end 0)) (len (- e b)))
+          (cond
+           ((and (= b (line-beginning-position)) (region-modifiable-p b e)) ;; Only whitespace
+            (delete-region b e))
+           ((= len 2) ;; Hard linebreak
+            (goto-char e))
+           ((and (= len 1) (region-modifiable-p b e)) ;; Single whitespace
+            (delete-region b e))
+           ((and (> len 2) (region-modifiable-p (+ b 2) e)) ;; 2 + extra
+            (delete-region (+ b 2) e))
+           (t (goto-char e))))))))
+
+(defun luk-markdown-hook-func ()
+  ;; Markdown lines can get long (e.g. with tables), and having a
+  ;; table row broken across multiple lines makes it unreadable.
+  (setq truncate-lines t)
+
+  ;; Use trailing whitespace deletion on save that respects markdown
+  (setq luk-should-trim-whitespace nil)
+  (add-hook 'before-save-hook 'luk-markdown-delete-trailing-whitespace nil 'make-local))
 
 (defun luk-markdown-setup ()
   (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
   ;; Lines can get long (e.g. tables)
-  (add-hook 'markdown-mode-hook 'luk-markdown--line-wrap-off))
+  (add-hook 'markdown-mode-hook 'luk-markdown-hook-func))
--- luk-whitespace.el
+++ luk-whitespace.el
@@ -2,20 +2,29 @@
 
 (provide 'luk-whitespace)
 
+(defvar-local luk-should-trim-whitespace t
+  "Controls whether ‘luk-maybe-trim-whitespace’ should trim.
+
+The main use of this variable is to allow excluding whitespace
+trimming from specific modes when ‘luk-maybe-trim-whitespace’ has
+been added to the ‘before-save-hook’. The variable should in that case
+be set to nil in the mode-hook for that mode.")
+
 (defun luk-squash-consecutive-blank-lines ()
-  "Turn consecutive blank lines into a single blank line"
-  (save-excursion
-    (beginning-of-buffer)
-    (while (re-search-forward
-            "^\^j\^j+"
-            nil ; unbounded
-            t); no error
-      (replace-match "\^j"))))
+  "Turn consecutive blank lines into a single blank line."
+  (save-match-data
+    (save-excursion
+      (beginning-of-buffer)
+      (while (re-search-forward
+              "^\^j\^j+"
+              nil ; unbounded
+              t); no error
+        (replace-match "\^j")))))
 
 (defun luk-maybe-trim-whitespace ()
-  "Trim trailing (and some other) whitespace depending on mode"
-  (if (not (string= major-mode "markdown-mode"))
-      (delete-trailing-whitespace))
+  "Trim trailing (and some other) whitespace in buffer."
+  (when luk-should-trim-whitespace
+    (delete-trailing-whitespace))
   (if (string-suffix-p ".ts" (buffer-file-name))
       ;; tslint doesn't allow multiple blank lines
       (luk-squash-consecutive-blank-lines)))
