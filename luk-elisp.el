f0135a7102b086d0b7961dc77ac1b50a5c2b79c4
lukas-ke <lukas-ke@users.noreply.github.com>
2022-10-05 06:58:57 +0200

    Add context hydras for emacs-lisp
    Add work-in-progress quality hydras for emacs lisp.

--- luk-elisp.el
+++ luk-elisp.el
@@ -3,6 +3,7 @@
 (provide 'luk-elisp)
 (require 'luk-util)
 (require 'luk-tab-complete)
+(require 'luk-hydra)
 
 (defun luk-elisp-insert-boilerplate ()
   "Insert magic comment for utf-8-coding and lexical binding"
@@ -50,7 +51,32 @@ handle minibuffer or other modes."
       (if company-mode (company-complete) (dabbrev-expand nil))))
    (t (indent-for-tab-command))))
 
+
+
+(defhydra luk-elisp-function-hydra (:hint nil :foreign-keys warn)
+  (format "\
+Main ➤ %s %%-30(symbol-at-point)^^^^^^^^^^^^^^ _._: up
+^─^───────────────────────────────────────────────────
+_g_: find definitions _G_: other window
+_d_: describe function"
+          (luk-caption "function"))
+  ("." (luk-hydra-push 'luk-org-hydra/body "org") :exit t)
+  ("g" xref-find-definitions :exit t)
+  ("G" xref-find-definitions-other-window :exit t)
+  ("d" (describe-function (symbol-at-point)) :exit t)
+  ("q" nil :exit t))
+
+;; Context hydra
+(defun luk-elisp-context-hydra ()
+  (interactive)
+  (let ((sym (symbol-at-point)))
+    (cond ((functionp sym) (luk-elisp-function-hydra/body))
+           (t (luk-hydra/body)))))
+
+
+;; Setup function
 (defun luk-elisp-setup ()
   (add-hook 'emacs-lisp-mode-hook 'luk-elisp--hook)
   (define-key emacs-lisp-mode-map (kbd "C-c g") 'xref-find-definitions-other-window)
-  (define-key emacs-lisp-mode-map (kbd "<tab>") 'luk-elisp-smart-tab))
+  (define-key emacs-lisp-mode-map (kbd "<tab>") 'luk-elisp-smart-tab)
+  (define-key emacs-lisp-mode-map (kbd "M-.") 'luk-elisp-context-hydra))
