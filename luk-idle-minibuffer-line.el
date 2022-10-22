3ac60bf742956f8fd7f2f6b69c00d7c2cead46ae
lukas-ke <lukas-ke@users.noreply.github.com>
2022-10-22 10:41:30 +0200

    Restore luk-idle-minibuffer-line on message clearing
    This is a bit of a hack to make my appointments and clock show more
    quickly after a message has been displayed.
    
    it will be interesting to see what kind of ill-effects I get from it.

--- luk-idle-minibuffer-line.el
+++ luk-idle-minibuffer-line.el
@@ -29,6 +29,34 @@ APPT is an appointment entry from the `appt-time-msg-list'."
                         'face '(:weight bold))
           (format "Next appt: %s" (car (cdr next-appt))))))))
 
+(defun luk-clear-to-idle-minibuffer-line ()
+  "Clear the minibuffer message and restore display of `minibuffer-line--buffer'.
+
+For use with the `clear-message-function'
+This function restores the content managed by
+`minibuffer-line-mode' immediately, without waiting for the
+`minibuffer-line--timer'.
+
+For example in `emacs-lisp-mode' with `eldoc-mode', documentation
+is shown in the echo-area for symbols at point. When point moves
+out, the messages are cleared. If the normal
+`clear-minibuffer-message' is used, this will leave the
+minibuffer area blank until the \"minibuffer-line\" is
+redisplayed via it's timer, while this function instead restores
+the display to the current minibuffer-line content, without
+updating it - that is still done by the timer)"
+  (clear-minibuffer-message)
+  (with-current-buffer minibuffer-line--buffer
+    ;; *Somehow* modifying the `minibuffer-line--buffer' causes it to be
+    ;; displayed in the minibuffer area. I could call minibuffer-line--update,
+    ;; but that's more expensive.
+    ;; TODO: There has to be a better way to redisplay the buffer
+    ;; than inserting and deleting a character
+    (goto-char 0)
+    (insert "x")
+    (goto-char 0)
+    (delete-char 1)))
+
 (defun luk-idle-minibuffer-line-enable ()
   "Display time and appointments in minibuffer."
   (interactive)
@@ -48,6 +76,7 @@ APPT is an appointment entry from the `appt-time-msg-list'."
                          1) ? )
          time-string)))))
   (face-spec-set 'minibuffer-line '((t :inherit default)))
+  (setq clear-message-function #'luk-clear-to-idle-minibuffer-line)
   (minibuffer-line-mode))
 
 (provide 'luk-idle-minibuffer-line)
