52918794d72356424605aec86d49f76e744d796f
lukas-ke <lukas-ke@users.noreply.github.com>
2022-10-23 15:21:19 +0200

    Smarter beginning of line

--- luk-init.el
+++ luk-init.el
@@ -178,6 +178,12 @@
   ;; auto-completing customization widgets.
   (define-key custom-field-keymap (kbd "C-<tab>") 'widget-complete))
 
+(with-eval-after-load 'help-mode
+  ;; Restore tab-navigation in help.
+  ;; luk-tab-complete eats it, due to hel-mode.el only defining TAB,
+  ;; not <tab>.
+  (define-key help-mode-map (kbd "<tab>") 'forward-button))
+
 ;; Note, only binding "<tab>", not "TAB", since org-cycle forwards to
 ;; the global TAB-bind sometimes, and luk-tab-complete-smart-tab
 ;; forwards to org-cycle which can lead to infinite recursion
@@ -472,6 +478,34 @@ modes (like `ido-mode') or falls back on `completing-read'
 (require 'luk-restore-killed-buffer)
 (luk-restore-killed-buffer-setup)
 (global-set-key (kbd "C-S-t") (lambda () (interactive) (luk-restore-killed-buffer-file)))
+
+
+;; https://emacsredux.com/blog/2013/05/22/smarter-navigation-to-the-beginning-of-a-line/
+(defun smarter-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+
+      Move point to the first non-whitespace character on this line.
+      If point is already there, move to the beginning of the line.
+      Effectively toggle between the first non-whitespace character and
+      the beginning of the line.
+
+      If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+      point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
+;; remap C-a to `smarter-move-beginning-of-line'
+(global-set-key (kbd "C-a") 'smarter-move-beginning-of-line)
 
 
 (provide 'luk-init)
