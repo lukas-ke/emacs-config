716fbcd938e61fde3dfe2ae72300871849cc9a51
lukas-ke <lukas-ke@users.noreply.github.com>
2022-10-24 08:23:19 +0200

    luk-insert-closing-delimiter: Support lua [[, python """

--- luk-insert-closing-delimiter.el
+++ luk-insert-closing-delimiter.el
@@ -3,10 +3,28 @@
 ;; delimiter for any open delimiter (like ({[")
 
 (defun luk--opposite-delimiter (delimiter)
+  "Return the opposite delimiter for DELIMITER.
+
+DELIMITER should be some brace type."
+  (cl-assert (stringp delimiter))
   (cond ((string-equal delimiter "{") "}")
         ((string-equal delimiter "(") ")")
         ((string-equal delimiter "[") "]")
-        (t "")))
+        (t (error "Unknown delimiter %s" delimiter))))
+
+(defun luk--opposite-string-delimiter (delimiter)
+  "Return the closing delimiter for string-delimiter DELIMITER.
+
+If DELIMITER is t, this will be the generic string delimiter for
+the major mode, if known."
+  (cond
+   ((characterp delimiter) (make-string 1 delimiter))
+   ((and (booleanp delimiter) delimiter)
+    (cond
+     ((eq major-mode 'lua-mode) "]]")
+     ((eq major-mode 'python-mode) "\"\"\"")
+     (t (error "Generic string delimiter for %s unknown" major-mode))))
+   (t (error "Unexpected delimiter type for %s" delimiter))))
 
 (defun luk-insert-closing-delimiter ()
   "Insert the closing delimiter for the last open delimiter at point.
@@ -19,9 +37,9 @@ insert \"}\", and for successive invocations \"]\" then \")\"."
   (interactive)
   (let* ((ppss (syntax-ppss))
          (open-paren-indexes (nth 9 ppss))
-         (quote-char (nth 3 ppss)))
-    (if quote-char
-        (insert (make-string 1 quote-char))
+         (in-string (nth 3 ppss)))
+    (if in-string
+        (insert (luk--opposite-string-delimiter in-string))
       (if open-paren-indexes
           (let ((delim (buffer-substring (car (last open-paren-indexes)) (+ 1 (car (last open-paren-indexes))))))
             ;; TODO: This doesn't say e.g. Matches: like when inserting a parenthesis normally.
