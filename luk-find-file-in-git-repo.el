083f16a8380244f69502c49fbaca1e1e8781c26a
lukas-ke <lukas-ke@users.noreply.github.com>
2022-10-25 06:49:33 +0200

    Fix luk-find-file-in-git-repo when not in root folder

--- luk-find-file-in-git-repo.el
+++ luk-find-file-in-git-repo.el
@@ -9,10 +9,10 @@
 (defun luk-find-file-in-git-repo ()
   "Find files in the current repository with completion."
   (interactive)
-  (find-file
-   (completing-read
-    "Find repository file: "
-    (let ((default-directory (luk-find-git-repo default-directory)))
+  (let ((default-directory (luk-find-git-repo default-directory)))
+    (find-file
+     (completing-read
+      "Find repository file: "
       (butlast (split-string (shell-command-to-string "git ls-files") "\n"))))))
 
 (provide 'luk-find-file-in-git-repo)
