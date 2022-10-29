a614087d9c544196a7985ef4290676749ce5d82e
lukas-ke <lukas-ke@users.noreply.github.com>
2022-10-29 20:36:33 +0200

    Add hydra for cycling commit messages

--- luk-magit.el
+++ luk-magit.el
@@ -16,6 +16,9 @@ initializing magit takes some time."
   (magit-status)
   (message nil))
 
+
+;;Rebase heading
+
 (defun luk-magit-rebase-show-rebase-heading ()
   "Indicate the reversed commit-order in header in rebase-todo.
 
@@ -29,7 +32,12 @@ file after picking the end commit in the normal log-order."
 
 (add-hook 'git-rebase-mode-hook #'luk-magit-rebase-show-rebase-heading)
 
+
+;; Stop `luk-tab-complete' from ruining cycling in the magit status
+;; buffer.
+
 (defun luk-magit-tab-cycle-heading ()
+  "Function for `luk-tab-complete-custom'"
   (interactive)
   (call-interactively #'magit-section-toggle)
   t)
@@ -38,9 +46,46 @@ file after picking the end commit in the normal log-order."
   (setq luk-tab-complete-custom #'luk-magit-tab-cycle-heading))
 
 (add-hook 'magit-mode-hook #'luk--on-magit-mode)
+
+;; Commit hydra and friends
 
+(defun luk-diff-while-committing ()
+  "Show the diff for the worked-on commit without asking to save.
+
+As far as I know, saving files won't have any effect on the diff
+for the ongoing commit."
+  (interactive)
+  (let ((magit-save-repository-buffers nil))
+    (magit-diff-while-committing)))
+
+(defhydra luk-git-commit-hydra (:hint nil
+                               :foreign-keys warn
+                               :pre (setq hydra-amaranth-warn-message "Invalid key.")
+                               :post (setq hydra-amaranth-warn-message luk-hydra-amaranth-original-message))
+  (format "\
+Main ➤ %s      _._: up
+^─^──────────────────────────
+_s_ Save message
+_p_ Previous message
+_n_ Next message
+_d_ Show diff
+
+_q_ Close menu
+"
+          (luk-caption "Commit"))
+  ("." (luk-hydra-push 'luk-git-commit-hydra/body "git-commit") :exit t)
+  ("s" (git-commit-save-message) :exit t)
+  ("p" (git-commit-next-message 1) :exit nil)
+  ("n" (git-commit-next-message 1) :exit nil)
+  ("d" (luk-diff-while-committing) :exit t)
+  ("q" nil :exit t))
+
+
 (with-eval-after-load 'magit
-  ;; Alternative to q
-  (define-key magit-blob-mode-map (kbd "k") #'magit-kill-this-buffer))
+  ;; Alternative to q for revision buffers (blobs)
+  (define-key magit-blob-mode-map (kbd "k") #'magit-kill-this-buffer)
+
+  ;; Hydra for commit messages
+  (define-key git-commit-mode-map (kbd "M-.") #'luk-git-commit-hydra/body))
 
 (provide 'luk-magit)
