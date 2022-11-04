232bbefe7310e29c32de08a211681009a7ca1d97
lukas-ke <lukas-ke@users.noreply.github.com>
2022-11-04 20:20:53 +0100

    Add elisp mode hydra

--- luk-elisp.el
+++ luk-elisp.el
@@ -1,20 +1,34 @@
 ;;; -*- coding: utf-8; lexical-binding: t -*-
 
-(provide 'luk-elisp)
 (require 'luk-util)
 (require 'luk-tab-complete)
 (require 'luk-hydra)
+(require 'flycheck)
 
 (defun luk-elisp-insert-boilerplate ()
-  "Insert magic comment for utf-8-coding and lexical binding"
+  "Insert magic comment for utf-8-coding and lexical binding."
   (interactive)
   (save-excursion
     (goto-char (point-min))
     (insert ";;; -*- coding: utf-8; lexical-binding: t -*-\n\n")))
 
+
+;; Mode hydra
+(defhydra luk-elisp-mode-hydra (:hint nil :foreign-keys warn :exit nil)
+  "
+_f_ Flycheck mode %-3s`flycheck-mode
+_e_ Flycheck errors
+_l_ Lint buffer
+_q_ Quit"
+  ("f" (lambda () (interactive) (flycheck-mode 'toggle)) :exit nil)
+  ("e" #'flycheck-list-errors :exit t)
+  ("l" #'elint-current-buffer :exit t)
+  ("q" nil :exit t))
 
+
 ;; Context hydra
 (defun luk-elisp-show-context-hydra ()
+  "Show context hydra for current position."
   (interactive)
   (let ((sym (symbol-at-point)))
     (if (functionp sym)
@@ -23,9 +37,8 @@
 
 (defun luk-elisp--hook()
   "Run luk-elisp-insert-boilerplate for new Emacs-lisp files."
-
   (setq luk-context-hydra #'luk-elisp-show-context-hydra)
-
+  (setq luk-mode-hydra #'luk-elisp-mode-hydra/body)
   (when (require 'company nil 'noerror)
     (company-mode))
 
@@ -40,7 +53,6 @@
     (forward-line)))
 
 
-
 (defhydra luk-elisp-function-hydra (:hint nil :foreign-keys warn)
   (format "\
 Main ➤ %s %%-30(symbol-at-point)^^^^^^^^^^^^^^ _._: up
@@ -57,6 +69,9 @@ _d_: describe function"
 
 ;; Setup function
 (defun luk-elisp-setup ()
+  "Configure emacs-lisp-mode."
   (add-hook 'emacs-lisp-mode-hook 'luk-elisp--hook)
   (define-key emacs-lisp-mode-map (kbd "C-c g") 'xref-find-definitions-other-window)
   (define-key emacs-lisp-mode-map (kbd "<tab>") 'luk-tab-complete-smart-tab))
+
+(provide 'luk-elisp)
