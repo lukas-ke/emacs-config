4f7f8aa847240e510bfe63407fc456ae173820a7
lukas-ke <lukas-ke@users.noreply.github.com>
2022-11-08 01:01:32 +0100

    luk-completing-read-multiple: disable fido-mode meanwhile

--- luk-minibuffer.el
+++ luk-minibuffer.el
@@ -4,6 +4,16 @@
 
 (provide 'luk-minibuffer)
 
+(defmacro luk/without-fido (&rest body)
+  "Disable fido-mode for the duration of BODY."
+  (if (not fido-mode)
+      `(progn ,@body)
+    `(unwind-protect
+         (progn
+           (fido-mode -1)
+           ,@body)
+       (fido-mode))))
+
 (defun luk-completing-read-multiple
     (PROMPT
      TABLE
@@ -22,22 +32,14 @@
         (crm-local-completion-map (copy-keymap crm-local-completion-map)))
     ;; Insert space when pressed, instead of triggering completion
     (define-key crm-local-completion-map " " 'self-insert-command)
-    (completing-read-multiple PROMPT TABLE PREDICATE REQUIRE-MATCH INITIAL-INPUT HIST DEF INHERIT-INPUT-METHOD)))
-
-;; Test function
-(defun luk-test-completing-read-multiple ()
-  (interactive)
-  (luk-completing-read-multiple "Prompt text: " '("Hello" "World")))
-
-;; Test function
-(defun luk-test-completing-read-multiple-f ()
-  (interactive)
-  (luk-completing-read-multiple
-   "Prompt text: "
-   (lambda (STRING PREDICATE whatev)
-     ;; Nil to say no-match.
-     ;; These completions replace strings
-     (message "waa %s" (buffer-string)) ;; content of minibuffer (including prompt :(..)
-     (if (string-equal STRING "flarf")
-         "florfo"
-       "flarf"))))
+    ;; Run without fido-mode, otherwise the history doesn't work
+    (luk/without-fido
+     (completing-read-multiple
+      PROMPT
+      TABLE
+      PREDICATE
+      REQUIRE-MATCH
+      INITIAL-INPUT
+      HIST
+      DEF
+      INHERIT-INPUT-METHOD))))
